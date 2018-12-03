74302a25d85e261b8990f4574cf86b10ec063ade
SZYMON <klimczukszymon@gmail.com>
2018-12-03 21:03:48 +0100

    first commit

---
+++ PEA_PROJECT_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PEA_PROJECT_2
+//
+//  Created by Szymon Klimczuk on 03/12/2018.
+//  Copyright © 2018 Szymon Klimczuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
